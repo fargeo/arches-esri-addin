d923eb2af726e28be145d3b45013a9611c44cb6d
njkim <nkim@fargeo.com>
2020-03-20 10:07:51 -0700

    Add a dockpane to save the resources back to arches

---
+++ ProtoType/Dockpane1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ProtoType
+{
+    /// <summary>
+    /// Interaction logic for Dockpane1View.xaml
+    /// </summary>
+    public partial class Dockpane1View : UserControl
+    {
+        public Dockpane1View()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("You Clicked the Save Button in the Save Resources Dockpane");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("You Clicked the Cancel Button in the Save Resources Dockpane");
+        }
+    }
+}
